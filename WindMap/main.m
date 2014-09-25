31b86f79264349ef5bb89f35ef3d62a4e1abc1d0
谢传奇 <xiechuanqi@123feng.com>
2014-09-25 16:19:39 +0800

    Initial Commit

---
+++ WindMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WindMap
+//
+//  Created by Zombie on 9/25/14.
+//  Copyright (c) 2014 Xcqwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ WindMapTests/WindMapTests.m
@@ -0,0 +1,34 @@
+//
+//  WindMapTests.m
+//  WindMapTests
+//
+//  Created by Zombie on 9/25/14.
+//  Copyright (c) 2014 Xcqwan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WindMapTests : XCTestCase
+
+@end
+
+@implementation WindMapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
